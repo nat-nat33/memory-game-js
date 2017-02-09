55897435c7e48d1db7f076b9a631ea8a2d841cc2
Natalie Macias <nramirez787@gmail.com>
2017-02-08 17:06:11 -1000

    adding card images

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Hello');
+//adds card images into array
+var cardImage = [];
+cardImage[0] = '/assets/zombie.png';
+cardImage[1] = '/assets/vampire.jpg';
+cardImage[2] = '/assets/ghost.jpg';
+cardImage[3] = '/assets/zombie.png';
+cardImage[4] = '/assets/vampire.jpg'
+cardImage[5] = '/assets/ghost.jpg';
