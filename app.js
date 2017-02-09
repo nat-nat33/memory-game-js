ce23f788a3c1951dfb62d58162e3e993729d549d
Natalie Macias <nramirez787@gmail.com>
2017-02-08 17:15:30 -1000

    adding counters for clicks, matches, and selection

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//adds card images into array
+//adds card images for flipped cards into array
 var cardImage = [];
 cardImage[0] = '/assets/zombie.png';
 cardImage[1] = '/assets/vampire.jpg';
@@ -6,3 +6,11 @@ cardImage[2] = '/assets/ghost.jpg';
 cardImage[3] = '/assets/zombie.png';
 cardImage[4] = '/assets/vampire.jpg'
 cardImage[5] = '/assets/ghost.jpg';
+
+var clicks = 0; //counter for how many selections have been made in each turn
+var firstSelect; //stores index of the first selection
+var secondSelect; //stores index of the second selection
+
+var matches = 0; //counter for matches made
+var cardBack = "/assets/question.jpg"; //displays back of the card when flipped back over
+
